2a86c6b3511c9785744b76d9a125466c524eb6e3
Nick Dimiduk <ndimiduk@gmail.com>
2010-08-28 10:43:22 -0700

    now with clojars

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject lein-hadoop "1.0.0"
   :description "A leiningen plugin to build jars for hadoop."
   :dev-dependencies [[org.clojure/clojure "1.1.0"]
-		     [org.clojure/clojure-contrib "1.1.0"]])
+		     [org.clojure/clojure-contrib "1.1.0"]
+		     [lein-clojars "0.6.0"]])
